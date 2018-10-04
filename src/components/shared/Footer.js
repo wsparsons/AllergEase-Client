82cb54d3abc1532ebb7b64e2f83a899107f4ffa5
Wendy Parsons <wsparsons@gmail.com>
2018-10-04 02:50:14 -0700

    setting up responsive design

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,46 +1,38 @@
 import React, { Component } from "react";
-import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Icon
-} from "semantic-ui-react";
+import { Container, Grid, Header, List, Segment } from "semantic-ui-react";
 
 class Footer extends Component {
   render() {
     return (
-      <Segment
-        inverted
-        vertical
-        // style={{ margin: "2em 0em 0em", padding: "2em 0em" }}
-      >
-        <Container textAlign="center">
-          <Icon
-            inverted
-            color="black"
-            name="barcode"
-            style={{ marginRight: "1.5em" }}
-          />
-          <List horizontal inverted divided link>
-            <List.Item as="a" href="#">
-              Site Map
-            </List.Item>
-            <List.Item as="a" href="#">
-              Contact Us
-            </List.Item>
-            <List.Item as="a" href="#">
-              Terms and Conditions
-            </List.Item>
-            <List.Item as="a" href="#">
-              Privacy Policy
-            </List.Item>
-          </List>
+      <Segment inverted vertical style={{ padding: "5em 0em" }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="About" />
+                <List link inverted>
+                  <List.Item as="a">Sitemap</List.Item>
+                  <List.Item as="a">Contact Us</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Services" />
+                <List link inverted>
+                  <List.Item as="a">Banana Pre-Order</List.Item>
+                  <List.Item as="a">DNA FAQ</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header as="h4" inverted>
+                  Footer Header
+                </Header>
+                <p>
+                  Extra space for a call to action inside the footer that could
+                  help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
       </Segment>
     );
