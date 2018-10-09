708ee8b2f8583958de610f1d3578094d2e195d0b
Wendy Parsons <wsparsons@gmail.com>
2018-10-08 22:44:06 -0700

    added get token from local storage in search model

--- src/models/search.models.js
+++ src/models/search.models.js
@@ -2,6 +2,9 @@ import axios from "axios";
 const BASE_URL = `https://allergease.herokuapp.com`;
 
 const search = async (userId, barcode) => {
+  let token = localStorage.getItem("AllergEase Token");
+  if (!token) return false;
+  
   const response = await axios(`${BASE_URL}/api/users/${userId}/barcode`, {
     method: "POST",
     headers: {
